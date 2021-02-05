cd3327ddcff600b0c52ea7dd56b873464e4c0ffe
lizhenguo <lizhenguo@kobolink.com>
2021-02-05 18:40:03 +0800

    nothing

--- app/src/main/java/com/quxiangtech/reference/Soft.java
+++ app/src/main/java/com/quxiangtech/reference/Soft.java
@@ -5,7 +5,6 @@ import java.lang.ref.SoftReference;
 public class Soft {
 
     public static void main(String[] args) {
-        ThreadLocal
         SoftReference<Integer> integerSoftReference = new SoftReference<>(1);
     }
 }
